65a27bd086352077358b2e4d929852fec6ac70a7
Interstellar-code <interstellar.consulting@gmail.com>
2025-03-27 10:41:05 +0100

    test

--- wallos-laravel/routes/web.php
+++ wallos-laravel/routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Test route
+Route::get('/test', function() {
+    return 'Laravel is working!';
+});
+
+// Authentication Routes
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
